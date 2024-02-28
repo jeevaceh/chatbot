3c55515d95c2571a58ac6752751e50277ee9bef1
JEEVACEH <jeevaceh@gmail.com>
2024-02-28 21:29:22 +0530

    live

--- src/openai.js
+++ src/openai.js
@@ -4,7 +4,8 @@ const openai = new OpenAI({
     apiKey:process.env.React_App_GPT_Key ,
     dangerouslyAllowBrowser: true 
   });
-   
+  
+  
  export async function chatFunction(message) {
     const completion = await openai.chat.completions.create({
       messages: [{"role": "assistant", "content": message}],
